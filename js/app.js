34e7e3dd6cc78162077284d1f2b31f27a4f93cd4
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-02-01 19:13:31 +0100

    FEAT added css and js code cleaned

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@
 //                 else if > 65
 //                 else no discount
 
+
+// took the elemets from HTML
 const priceGeneratorElement = document.querySelector('#form-container input[name="price-generator"]')
 
 const travelDistanceElement = document.querySelector('#form-container input[name="travel"]')
@@ -24,37 +26,63 @@ const userFullName = document.querySelector('#form-container input[name="full-na
 
 console.log (travelDistanceElement, ageSelectorElement, priceGeneratorElement)
 
+// add event
 priceGeneratorElement.addEventListener('click', function() {
 
 console.log(travelDistanceElement.value, ageSelectorElement.value)
 
+// add price rate
 
 const PRICE_PER_KM = 0.21
 
+// add passenger name
+
 let fullName = userFullName.value
+
+// stamp passenger name on HTML
+
+const passengerName = document.querySelector('#passenger-name')
+passengerName.innerHTML = fullName
+
+// stamp train carriage number on HTML
+
+const trainCarriage = document.querySelector('#train-carriage')
+trainCarriage.innerHTML = parseInt(Math.random() * 9) + 1
+
+// stamp train code on HTML
+
+const trainCode = document.querySelector('#train-code')
+trainCode.innerHTML = parseInt(Math.random() * 9999) + 1
+
+// add the "travel" value
 
 let travel = travelDistanceElement.value
     if (travel < 0 || isNaN (travel)) {
         travel = 'Not valid'
     }
 
+// add total price
 
 const totalPrice = travel * PRICE_PER_KM;
     console.log(totalPrice)
-
-
+    
 let Euro = new Intl.NumberFormat('en-DE', {
     style: 'currency',
     currency: 'EUR',
 })
     
+// add the "age-selector" value
 
 let ageToSelect = ageSelectorElement.value
 
     if (ageToSelect == "young") {
 
+        // get the price of the discount 
+
         const youngDiscount = totalPrice * 0.2
         console.log(youngDiscount)
+
+        // get the price difference
 
         const yourPrice = document.getElementById('total-price')
 
@@ -62,14 +90,23 @@ let ageToSelect = ageSelectorElement.value
                 yourPrice.innerHTML = 'Not valid'
             }
 
+            // stamp the new price on HTML 
+
             else {
                 yourPrice.innerHTML = Euro.format(totalPrice - youngDiscount)
                 console.log(yourPrice)
             }
-            
+
+        // stamp the type of ticket in HTML 
+        
+        const ticketType = document.getElementById('ticket-type')
+        ticketType.innerHTML = 'Young Ticket'
+        
     }
 
     else if (ageToSelect == "middle") {
+
+        // get the price without discount 
 
         const noDiscount = totalPrice
 
@@ -79,16 +116,27 @@ let ageToSelect = ageSelectorElement.value
             yourPrice.innerHTML = 'Not valid'
         }
 
+        // stamp the price on HTML
+
         else {
             yourPrice.innerHTML = Euro.format(noDiscount)
         }
+
+        // stamp the type of ticket in HTML 
+
+        const ticketType = document.getElementById('ticket-type')
+        ticketType.innerHTML = 'Standard Ticket'
 
     }
 
     else if (ageToSelect == "old"){
 
+        // get the price of the discount
+
         const oldDiscount = totalPrice * 0.4
         console.log(oldDiscount)
+
+        // get the price difference
 
         const yourPrice = document.getElementById('total-price')
 
@@ -96,9 +144,16 @@ let ageToSelect = ageSelectorElement.value
                 yourPrice.innerHTML = 'Not valid'
             }
 
+            // stamp the new price on HTML
+
             else {
                 yourPrice.innerHTML = Euro.format(totalPrice - oldDiscount)
             }
+
+            // stamp the type of ticket in HTML 
+
+            const ticketType = document.getElementById('ticket-type')
+            ticketType.innerHTML = 'Over Ticket'    
     }
 
 })
